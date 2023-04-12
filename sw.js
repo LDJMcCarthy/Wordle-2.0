4ffaee34a283a63598a6bf4abcf6d5ca57b5dd19
LDJMcCarthy <bchh34@durham.ac.uk>
2023-04-12 06:26:12 +0100

    new sw

--- sw.js
+++ sw.js
@@ -76,7 +76,7 @@ self.addEventListener("install", (e) => {
     (async () => {
       const cache = await caches.open(cacheName);
       console.log("[Service Worker] Caching all: app shell and content");
-      await cache.addAll(contentToCache);
+      await cache.addAll(appShellFiles);
     })()
   );
 });
