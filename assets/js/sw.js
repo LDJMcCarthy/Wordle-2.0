d643969965a909bd34d8f75c9574670cdf9f14d6
LDJMcCarthy <bchh34@durham.ac.uk>
2023-04-12 02:12:58 +0100

    test sw 1

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -15,10 +15,10 @@
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
+    './',
     './index.html',
     './manifest.json',
     './assets/css/keyboard.css',
@@ -89,53 +89,43 @@ const PRECACHE_URLS = [
     './assets/fonts/zilla-slab-v11-latin-regular.woff2'
 ];
 
+// Registering the SW
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('./service-worker.js')
+           .then(function() { console.log('Service Worker Registered'); });
+}
+
 // The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+
+  e.waitUntil(
+    caches.open(PRECACHE).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(PRECACHE_URLS);
+    })
   );
 });
 
 // The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+  e.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== PRECACHE) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
       })
-    );
-  }
+  );
+
+  return self.clients.claim();
 });
+
 
 
 
